d2c35f8c858367c387cc5152cd11710826ee09f5
Moin Alam <moinalam010@gmail.com>
2021-08-26 14:15:12 +0600

    div

--- src/store/index.js
+++ src/store/index.js
@@ -18,18 +18,89 @@ export default createStore({
 
   },
   mutations: {
-    SET_COUNTRIES (state, desh){
+    SET_COUNTRIES (state, desh){ 
       state.countries = desh
-    }
+    },
+    SET_POPULATION (state, people){  
+      state.countries = people
+    },
+    SET_LOCATION (state, elaka){ 
+      state.countries = elaka
+    },
+    SET_FLAG (state, flag){   
+      state.countries = flag
+    },
+    SET_CURRENCIES (state, money){    
+      state.countries = money
+    },
+    SET_CAPITAL (state, rajdhani){      
+      state.countries = rajdhani
+    },
+    SET_DIALCODES (state, code){     
+      state.countries = code
+    },
+    SET_STATES (state, rajjo){     /* baki */
+      state.countries = rajjo
+    },
+    
   },
   actions: {
     getCountry({ commit }) {
       axios.get('https://countriesnow.space/api/v0.1/countries/currency')
         .then( (data)=> {
-          const desh = data.data.data;
+          const desh = data.data.data
           commit('SET_COUNTRIES', desh)
         })
-    }
+    },
+    getPopulation({ commit }) {
+      axios.get('https://countriesnow.space/api/v0.1/countries/population')
+        .then( (data)=> {
+          const population = data.data.data
+          commit('SET_POPULATION', population)
+        })
+    },
+    getLocation({ commit }) {
+      axios.get('https://countriesnow.space/api/v0.1/countries/positions')
+        .then( (data)=> {
+          const elaka = data.data.data
+          commit('SET_LOCATION', elaka)
+        })
+    },
+    getFlag({ commit }) {
+      axios.get('https://countriesnow.space/api/v0.1/countries/flag/images')
+        .then( (data)=> {
+          const flag = data.data.data
+          commit('SET_FLAG', flag)
+        })
+    },
+    getCurrencies({ commit }) {
+      axios.get('https://countriesnow.space/api/v0.1/countries/currency')
+        .then( (data)=> {
+          const money = data.data.data
+          commit('SET_CURRENCIES', money)
+        })
+    },
+    getCapital({ commit }) {
+      axios.get('https://countriesnow.space/api/v0.1/countries/capital')
+        .then( (data)=> {
+          const rajdhani = data.data.data
+          commit('SET_CAPITAL', rajdhani)
+        })
+    },
+    getDialCode({ commit }) {
+      axios.get('https://countriesnow.space/api/v0.1/countries/codes')
+        .then( (data)=> {
+          const code = data.data.data
+          commit('SET_DIALCODES', code)
+        })
+    },
+    getPopulation({ commit }) {
+      axios.get('https://countriesnow.space/api/v0.1/countries/states')
+        .then( (data)=> {
+          const rajjo = data.data.data
+          commit('SET_STATES', rajjo)
+        })
+    },
   },
  
   modules: {
--- src/views/countryList.vue
+++ src/views/countryList.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="countrylist">
-    <h1> Heading</h1>
 
-    <button v-for="country in countries" >
-      {{country.name}}
-    </button>
-    
+    <h1> World domination targets</h1>
+
+
+    <div v-for="country in countries">
+
+      router-link
+
+      <p> {{country.name}} </p>
+    </div>
+
+
   </div>
-
-
 </template>
+
+
 
 <script>
 
@@ -25,7 +31,8 @@ export default {
   computed:{
     countries: function () {
       return this.$store.state.countries
-    }
+    },
+
   },
 
 }
